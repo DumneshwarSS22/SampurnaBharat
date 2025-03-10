ed748b8984407429c23f9199aee844217386879c
DumneshwarSS22 <dumaneshwar22@gmail.com>
2025-03-10 21:56:32 +0530

    first commit

--- Sampurna Bharat/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const contactForm = document.querySelector("form");
-    contactForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        alert("Thank you for your message! We will get back to you soon.");
-        contactForm.reset();
-    });
-});
--- Sampurna Bharat/js/user_dashboard.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for future interactivity (optional)
-// For example, to open a modal for viewing health records
-document.addEventListener('DOMContentLoaded', () => {
-    const viewRecordsButton = document.querySelector('button');
-    viewRecordsButton.addEventListener('click', () => {
-        alert('Fetching medical records...');
-    });
-});
